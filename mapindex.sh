0fc13272efa1c8d8fec9aef7449e6390b1253368
Gregory Danielson <gregory.danielson3@gmail.com>
2023-03-07 16:18:12 -0600

    Fix mapindex subdir to `mindmap` not `mind-map`

--- mapindex.sh
+++ mapindex.sh
@@ -3,8 +3,8 @@ cat templates/map_header.md
 
 cd pages || exit
 
-tree=$(tree -tf --noreport --charset ascii mind-map* |
-       sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g' -e 's/\.md/\.html/g' -e 's/mind-map\///g'|
-       tail +2)
+tree=$(tree -tf --noreport --charset ascii mindmap* |
+    sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g' -e 's/\.md/\.html/g' -e 's/mindmap\///g'|
+tail +2)
 
 echo -n "$tree"
